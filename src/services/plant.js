f9e03a6a342b912037b9fede255934cebc1084c7
BWBMaxime <83752129+BWBMaxime@users.noreply.github.com>
2023-04-27 16:32:47 +0200

    update by methode update

--- src/services/plant.js
+++ src/services/plant.js
@@ -1,4 +1,4 @@
-import { sequelize } from '../configurations/database.js'
+// import { sequelize } from '../configurations/database.js'
 import { NotFound } from '../globals/errors.js'
 import { Plant } from '../models/plant.js'
 
@@ -27,12 +27,12 @@ export default class PlantService {
    const plant = await Plant.update({ ...fields }, { where: { plant_id: id } })
    TODO : Le code si dessous ne gère pas les appostrophe lors d'un update dans une chaine ( TODO FRONT ) */
   async putPlantById ({ fields, id }) {
-    const [results, metadata] = await sequelize.query(`UPDATE PLANT SET name_plant='${fields.name_plant}', instructions_plant='${fields.instructions_plant}', type_plant='${fields.type_plant}',status_plant='${fields.status_plant}' WHERE plant_id = ${id}`)
-    // if (!plant) {
-    //   throw new NotFound('La plant n\'existe pas.')
-    // }
-    console.log(results, metadata)
-    return [results, metadata]
+    const plant = await Plant.update({ ...fields }, { where: { plant_id: id } })
+    // const plant = await sequelize.query(`UPDATE PLANT SET name_plant='${fields.name_plant}', instructions_plant='${fields.instructions_plant}', type_plant='${fields.type_plant}',status_plant='${fields.status_plant}' WHERE plant_id = ${id}`)
+    if (!plant) {
+      throw new NotFound('La plant n\'existe pas.')
+    }
+    return plant
   }
 
   async deletePlantById ({ id }) {
