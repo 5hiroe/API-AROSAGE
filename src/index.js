a29f1905f17dea576d62ac0332f067787933571c
louis nebout <louisnebout34@gmail.com>
2023-07-09 21:41:54 +0200

    Intégration swagger en cours

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import configure from './configurations/configuration.js'
-import swaggerJsdoc from 'swagger-jsdoc';
-//import swaggerUi from 'swaggerUi';
+//import path from ('path');
+import swaggerJsdoc  from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import router from './routes/auth.js';
+//import routes from ('./routes/auth.js');
 
 
 
@@ -22,29 +24,104 @@ async function main () {
   });
 
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Arosaje',
-      version: '1.0.0',
-      description: 'Documentation de mon API',
+// swagger definition
+const swaggerDefinition = {
+  info: {
+    title: 'Node Swagger API',
+    version: '1.0.1',
+    description: 'Demonstrating how to describe a RESTful API with Swagger',
+  },
+  host: 'localhost:3000',
+  basePath: '/',
+  tags: [
+    {
+      name: 'Users',
+      description: 'Endpoints related to users',
+    },
+    {
+      name: 'Products',
+      description: 'Endpoints related to products',
+    },
+  ],
+  definitions: {
+    User: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'integer',
+        },
+        name: {
+          type: 'string',
+        },
+      },
+    },
+    Product: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'integer',
+        },
+        name: {
+          type: 'string',
+        },
+        price: {
+          type: 'number',
+        },
+      },
     },
   },
-  apis: ['../routes/auth.js'], // Spécifiez le chemin vers vos fichiers de routes ou de contrôleurs
+  paths: {
+    '/users': {
+      get: {
+        tags: ['Users'],
+        summary: 'Get all users',
+        responses: {
+          200: {
+            description: 'Successful response',
+          },
+        },
+      },
+    },
+    '/signup': {
+      post: {
+        tags: ['Products'],
+        summary: 'Post for signup',
+        responses: {
+          200: {
+            description: 'Successful response',
+          },
+        },
+      },
+    },
+  },
 };
 
-const specs = swaggerJsdoc(options);
+const options = {
+  swaggerDefinition,
+  apis: ['./routes/*.js'],
+};
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+const swaggerSpec = swaggerJsdoc(options);
 
-  //app.listen(process.env.PORT, (err) => {
-  //  if (err) {
-  //    console.log(err)
-  //  } else {
-  //    console.log(`Server Initialized ON ${process.env.PORT}`)
-  //  }
-  //})
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+app.get('/swagger.json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+
+//app.use('/', routes);
+//app.use(express.static(path.join(__dirname, 'public')));
+
+
+  app.listen(process.env.PORT, (err) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(`Server Initialized ON ${process.env.PORT}`)
+    }
+  })
 }
 
 main()
